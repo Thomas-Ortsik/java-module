3464d7effda7a3361cad2eb87ba8efe729a2f8eb
Thomas Ortsik <doctor.strangethumb@gmail.com>
2020-02-18 12:57:30 +0100

    commit for week 9, set1

---
+++ src/academy/everyonecodes/java/week8/reflection/exercise1/cutlery/Cutlery.java
@@ -0,0 +1,7 @@
+package academy.everyonecodes.java.week8.reflection.exercise1.cutlery;
+
+public interface Cutlery {
+    String getName();
+
+    boolean isUsedFor(String dish);
+}
---
+++ src/academy/everyonecodes/java/week8/reflection/exercise1/cutlery/Spoon.java
@@ -0,0 +1,13 @@
+package academy.everyonecodes.java.week8.reflection.exercise1.cutlery;
+
+public class Spoon implements Cutlery {
+    @Override
+    public String getName() {
+        return "Spoon";
+    }
+
+    @Override
+    public boolean isUsedFor(String dish) {
+        return dish.toLowerCase().equals("soup");
+    }
+}
